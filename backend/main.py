d5d81aaf2459d4d645f41dafa0b737d69243ca3b
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-05-20 01:39:06 +0530

    Add health check endpoint and configure rate limiting

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+# from routes.chat import router as router_chat 
+# from routes.upload import router as router_upload
+
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=get_remote_address)
+app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+# app.include_router(router_chat,"/chat")
+# app.include_router(router_upload,"/upload")
 
 
-app =FastAPI()
-
-@app.get("/")
-def hello():
-    return "hello myyself ddeep panchal"
+@app.get('/health')
+@limiter.limit("5/minute")
+def health(request :Request):
+    return {"Status":"Healthy"}
